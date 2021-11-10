ad7a1d2333a8c22be9e42462f3621cef4ebf0c8d
Marley Doughty <marleydoughty@yahoo.com>
2021-11-10 11:15:39 -0800

    removed commmented out code

--- js/main.js
+++ js/main.js
@@ -4,8 +4,6 @@ function getData(image) {
   xhr.open('GET', 'https://api.thecatapi.com/v1/images/search?limit=100&page=0');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    // console.log('xhr status:', xhr.status);
-    // console.log('xhr response:', xhr.response);
     for (var i = 0; i < xhr.response.length; i++) {
       var image = document.createElement('img');
       image.setAttribute('src', xhr.response[i].url);
