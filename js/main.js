379714b11e65f08a142f809613c7cc7e01e80215
Marley Doughty <marleydoughty@yahoo.com>
2021-11-09 17:57:15 -0800

    added function for API requests and basic html structure

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// var $images = document.querySelector('.images');
+// function getData(image) {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://api.thecatapi.com/v1/images/search?limit=100&page=0');
+//   xhr.responseType = 'json';
+//   xhr.addEventListener('load', function () {
+//     console.log('xhr status:', xhr.status);
+//     console.log('xhr response:', xhr.response);
+//     for (var i = 0; i < xhr.response.length; i++) {
+//       var image = document.createElement('img');
+//       image.setAttribute('src', xhr.response[i].url);
+//       $images.appendChild(image);
+//     }
+//   });
+//   xhr.send();
+// }
+
+// getData();
