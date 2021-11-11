f2fa897697736818e11a60f5566696db1cb7727f
Marley Doughty <marleydoughty@yahoo.com>
2021-11-10 17:05:34 -0800

    added column-classes and adjusted image layout, working on comment-input layout

--- js/main.js
+++ js/main.js
@@ -1,43 +1,60 @@
-// var $images = document.querySelector('.images');
+var $images = document.querySelector('.images');
 
-// function getData(image) {
-//   var xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://api.thecatapi.com/v1/images/search?limit=100&page=0');
-//   xhr.responseType = 'json';
-//   xhr.addEventListener('load', function () {
-//     for (var i = 0; i < xhr.response.length; i++) {
-//       var image = document.createElement('img');
-//       image.setAttribute('src', xhr.response[i].url);
-//       $images.appendChild(image);
+function getData(image) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.thecatapi.com/v1/images/search?limit=100&page=0');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    for (var i = 0; i < 9; i++) {
+      var imageContainer = document.createElement('div');
+      imageContainer.className = 'row flex-wrap width';
+      $images.appendChild(imageContainer);
 
-//       var commentSection = document.createElement('div');
-//       commentSection.className = 'row comment-section';
-//       $images.appendChild(commentSection);
+      // var columnFull = document.createElement('div');
+      // columnFull.className = 'column-full';
+      // imageContainer.appendChild(columnFull);
 
-//       var commentOutput = document.createElement('p');
-//       commentSection.appendChild(commentOutput);
+      var image = document.createElement('img');
+      image.setAttribute('src', xhr.response[i].url);
+      image.className = 'column-full';
+      imageContainer.appendChild(image);
 
-//       var form = document.createElement('form');
-//       commentSection.appendChild(form);
+      var commentSection = document.createElement('div');
+      commentSection.className = 'row comment-section align-center';
+      imageContainer.appendChild(commentSection);
 
-//       var commentInput = document.createElement('input');
-//       commentInput.setAttribute('type', 'text');
-//       form.appendChild(commentInput);
+      var columnHalf = document.createElement('div');
+      columnHalf.className = 'column-half';
+      commentSection.appendChild(columnHalf);
 
-//       var saveComment = document.createElement('input');
-//       saveComment.setAttribute('type', 'submit');
-//       form.appendChild(saveComment);
+      var commentOutput = document.createElement('p');
+      columnHalf.appendChild(commentOutput);
 
-//       var commentIconContainer = document.createElement('div');
-//       commentIconContainer.className = 'row comment-icon';
-//       $images.appendChild(commentIconContainer);
+      var form = document.createElement('form');
+      columnHalf.appendChild(form);
 
-//       var commentIcon = document.createElement('i');
-//       commentIcon.className = 'fas fa-comment';
-//       commentIconContainer.appendChild(commentIcon);
-//     }
-//   });
-//   xhr.send();
-// }
+      var commentInput = document.createElement('input');
+      commentInput.setAttribute('type', 'text');
+      commentInput.setAttribute('placeholder', 'Add comments here');
+      commentInput.className = 'comment-box';
+      form.appendChild(commentInput);
 
-// getData();
+      var saveComment = document.createElement('input');
+      saveComment.setAttribute('type', 'submit');
+      saveComment.setAttribute('value', 'Save');
+      saveComment.className = 'save-button';
+      form.appendChild(saveComment);
+
+      var commentIconContainer = document.createElement('div');
+      commentIconContainer.className = 'comment-icon column-half';
+      commentSection.appendChild(commentIconContainer);
+
+      var commentIcon = document.createElement('i');
+      commentIcon.className = 'fas fa-comment';
+      commentIconContainer.appendChild(commentIcon);
+    }
+  });
+  xhr.send();
+}
+
+getData();
